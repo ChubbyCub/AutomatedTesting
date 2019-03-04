092bafe32cd25e9fe0475e408811bd16e1c631aa
Elise Pham <elise.pham88@gmail.com>
2019-03-04 10:52:49 -0800

    completed tests for to_s method and attrreader methods

--- lib/card.rb
+++ lib/card.rb
@@ -7,11 +7,27 @@ class Card
   def initialize(value, suit)
     @value = value
     @suit = suit
+    if !(1..13).include?(value) 
+      raise ArgumentError.new("Invalid value")
+    end
 
-
+    if ![:hearts, :spades, :clubs, :diamonds].include?(suit)
+      raise ArgumentError.new("Invalid suit")
+    end
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    case value
+    when 1
+      return "Ace of #{suit.to_s}"
+    when 11
+      return "Jack of #{suit.to_s}"
+    when 12
+      return "Queen of #{suit.to_s}"
+    when 13
+      return "King of #{suit.to_s}"
+    else
+      return "#{value} of #{suit.to_s}"
+    end
   end
 end
--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -27,11 +27,28 @@ describe Card do
     it "to_s returns a readable String value logically for values 2-10" do
       # Test to ensure that to_s works for cards values 2-10
       # for example:  "2 of diamonds"
+
+      # Arrange
+      value = 2
+      suit = :diamonds
+      card = Card.new(value, suit)
+
+      # Act
+      text_result = card.to_s
+
+      # Assert
+      expect(text_result).must_equal "2 of diamonds"
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
-      # Test to ensure that to_s works for cards values 1, and 11-13
-      # For example: "Queen of hearts"
+      values = [1, 11, 12, 13]
+      names = ["Ace", "Jack", "Queen", "King"]
+      suits = [:hearts, :spades, :clubs, :diamonds]
+      
+      values.each_with_index do |value, index|
+        card = Card.new(value, suits[index])
+        expect(card.to_s).must_equal("#{names[index]} of #{suits[index]}")
+      end
     end
   end
 
@@ -39,11 +56,24 @@ describe Card do
 
     it "Can retrieve the value of the card using a `.value`." do
       # ensure that `.value works as expected`
+      values = [2, 4, 13, 1]
+      suits = [:hearts, :spades, :clubs, :diamonds]
+
+      values.each_index { |i|
+        card = Card.new(values[i], suits[i])
+        expect(card.value).must_equal values[i]
+      }
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
+      values = [2, 4, 13, 1]
+      suits = [:hearts, :spades, :clubs, :diamonds]
 
+      suits.each_index { |i|
+        card = Card.new(values[i], suits[i])
+        expect(card.suit).must_equal suits[i]
+      }
     end
   end
 
